3c48f0f315f3b0006fc319dd0599dca9f7d34b1f
Mladen Draganović <mladenoneacc@gmail.com>
2021-06-28 12:38:28 +0200

    🔧 Added source code

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="text-3xl" style="color: red">
+    This text should be green in dark mode!
+  </h1>
+  <h2 class="text-3xl" style="color: red">This text is green in dark mode!</h2>
+</template>
+
+<style lang="postcss">
+h1 {
+  @apply dark:!text-green-500;
+}
+@media (prefers-color-scheme: dark) {
+  h2 {
+    @apply !text-green-500;
+  }
+}
+</style>
